4f925462c65bf7d206422e7592d6dc63aacae9e2
lixiong <qqlixiongjob@163.com>
2025-05-15 11:16:40 +0800

    ......

--- tablayout/src/main/java/com/codbking/widget/view/a.kt
+++ tablayout/src/main/java/com/codbking/widget/view/a.kt
@@ -33,29 +33,6 @@ object a {
     @JvmStatic
     fun TextView.a(s: String?) {
         try {
-            when(s){
-                "EAR" ->{
-                    text = "平均需要量"
-                }
-                "RNI" ->{
-                    text = "推荐摄入量"
-                }
-                "AI" ->{
-                    text = "适宜摄入量"
-                }
-                "UL" ->{
-                    text = "可耐受最高摄入量"
-                }
-                "AMDR" ->{
-                    text = "宏量营养素可接受范围"
-                }
-                "PI" ->{
-                    text = "建议摄入量"
-                }
-                else ->{
-                    text = "特定建议值"
-                }
-            }
         } catch (_: Exception) {
         }
     }
@@ -64,12 +41,7 @@ object a {
     @JvmStatic
     fun HtmlTextView.b(s: String?) {
         try {
-            s?.replace("http://", "https://")?.let {
-                setHtml(
-                    it,
-                    HtmlHttpImageGetter(this)
-                )
-            }
+
         } catch (_: Exception) {
         }
     }
@@ -98,13 +70,7 @@ object a {
     @JvmStatic
     fun MyJzvdStd.c(s: String?, b: String?) {
         try {
-            setUp(b,"")
-            positionInList = 1
-            if (s?.isNotEmpty() == true) {
-                Glide.with(posterImageView.context)
-                    .load(s)
-                    .into(posterImageView)
-            }
+
         } catch (_: Exception) {
         }
     }
@@ -116,8 +82,7 @@ object a {
     @JvmStatic
     fun IView.d(s: String?, b: String?) {
         try {
-            setLeftValue(s)
-            setRightValue(b)
+
         } catch (_: Exception) {
         }
     }
@@ -129,42 +94,6 @@ object a {
     @JvmStatic
     fun View.sd(k: Int?, v: String?) {
         try {
-            when(this){
-                is TextView ->{
-                    when(k){
-                        1 ->{
-                            var h = v
-                            if (h?.contains(":") == true) {
-                                val a = h.split(":")[0]
-                                val b = h.split(":")[1]
-                                if (b == "00") {
-                                    h = a
-                                } else if (b.startsWith("0")) {
-                                    val c = b.substring(1)
-                                    val d = c.toFloat() / 60
-                                    val e =
-                                        DecimalUtils.decimalFormat((a.toFloat() + d).toString())
-                                    h = if (e.endsWith("0")) {
-                                        e.substring(0, e.length - 1)
-                                    } else {
-                                        e
-                                    }
-                                } else {
-                                    val d = b.toFloat() / 60
-                                    val e =
-                                        DecimalUtils.decimalFormat((a.toFloat() + d).toString())
-                                    h = if (e.endsWith("0")) {
-                                        e.substring(0, e.length - 1)
-                                    } else {
-                                        e
-                                    }
-                                }
-                            }
-                            text = h
-                        }
-                    }
-                }
-            }
         } catch (_: Exception) {
         }
     }
@@ -176,10 +105,7 @@ object a {
     @JvmStatic
     fun IView.e(s: Int?, b: String?) {
         try {
-            s?.toFloat()?.let { setCurrentProgress(it) }
-            if (b != null) {
-                setText(b)
-            }
+
         } catch (_: Exception) {
         }
     }
@@ -191,27 +117,18 @@ object a {
     @JvmStatic
     fun LineChart.f(hasWeek: Int?, hasBubble:Int?) {
         try {
-            if (hasBubble == View.GONE || hasBubble == View.INVISIBLE){
-                hasWeek?.let {
-                    visibility = it
-                }
-            }else{
-                hasBubble?.let {
-                    visibility = View.GONE
-                }
-            }
+
         } catch (_: Exception) {
         }
     }
 
     @BindingAdapter(
-        value = ["blood_sugar_avg", "blood_sugar_proportion","blood_sugar_fluctuate","blood_sugar_reachts","blood_sugar_high","blood_sugar_low","blood_sugar_normal"],
+        value = ["blood_sugar_avg", "blood_sugar_proportion","blood_sugar_fluctuate","blood_sugar_react","blood_sugar_high","blood_sugar_low","blood_sugar_normal"],
         requireAll = false
     )
     @JvmStatic
     fun IVGroup.g(avg: String?, proportion:String?, fluctuate:String?, reachts:String?, high:String?, low:String?, normal:String?) {
         try {
-            loadData(avg,proportion,fluctuate,reachts,high,low,normal)
         } catch (_: Exception) {
         }
     }
@@ -221,9 +138,8 @@ object a {
         requireAll = false
     )
     @JvmStatic
-    fun IVGroup.h(sleepLength: String?, lieinbedLength:String?) {
+    fun IVGroup.h(sleepLength: String?, length:String?) {
         try {
-            loadData(sleepLength, lieinbedLength)
         } catch (_: Exception) {
         }
     }
@@ -232,7 +148,6 @@ object a {
     @JvmStatic
     fun IVGroup.i(fat: String?) {
         try {
-            loadData(fat)
         } catch (_: Exception) {
         }
     }
@@ -241,7 +156,6 @@ object a {
     @JvmStatic
     fun IVGroup.j(diet: String?) {
         try {
-            loadData(diet)
         } catch (_: Exception) {
         }
     }
@@ -253,15 +167,7 @@ object a {
     @JvmStatic
     fun TextView.k(value:String?,unit: String?) {
         try {
-            value?.let {
-                text = if (it.isNotEmpty()){
-                    unit
-                } else {
-                    ""
-                }
-            }?: kotlin.run {
-                text = ""
-            }
+
         } catch (_: Exception) {
         }
     }
@@ -270,101 +176,61 @@ object a {
     @JvmStatic
     fun TextView.l(weight: String?) {
         try {
-            weight?.let {
-                text = if (it.isNotEmpty()){
-                    weight
-                }else{
-                    "--"
-                }
-            }?: kotlin.run {
-                text = "--"
-            }
+
         } catch (_: Exception) {
         }
     }
 
-    @BindingAdapter("plan_text_01")
+    @BindingAdapter("plan_text_one")
     @JvmStatic
     fun TextView.m(t: String?) {
         try {
-            t?.let {
-                text = if (t.length >= 16){
-                    t.substring(0,16)
-                }else{
-                    t
-                }
-            }
+
         } catch (_: Exception) {
         }
     }
 
-    @BindingAdapter("plan_text_02")
+    @BindingAdapter("plan_text_tow")
     @JvmStatic
     fun TextView.n(t: String?) {
         try {
-            t?.let {
-                text = if (t.length >= 34){
-                    t.substring(16,34)
-                }else if (t.length >= 16){
-                    t.subSequence(16,t.length)
-                }else{
-                    ""
-                }
-            }
+
         } catch (_: Exception) {
         }
     }
 
-    @BindingAdapter("plan_text_03")
+    @BindingAdapter("plan_text_three")
     @JvmStatic
     fun TextView.k(t: String?) {
         try {
-            t?.let {
-                text = if (t.length >= 53){
-                    t.substring(34,53)
-                }else if (t.length >= 34){
-                    t.substring(34,t.length)
-                }else{
-                    ""
-                }
-            }
+
         } catch (_: Exception) {
         }
     }
 
-    @BindingAdapter("plan_text_04")
+    @BindingAdapter("plan_text_four")
     @JvmStatic
     fun TextView.r(t: String?) {
         try {
-            t?.let {
-                text = if (t.length >= 53){
-                    t.substring(53,t.length)
-                }else{
-                    ""
-                }
-            }
+
         } catch (_: Exception) {
         }
     }
 
-    @BindingAdapter("plan_problem_01")
+    @BindingAdapter("plan_problem_one")
     @JvmStatic
     fun TextView.u(t: String?) {
         try {
-            t?.let {
-                text = t.substring(0,1)
-            }
+
         } catch (_: Exception) {
         }
     }
 
-    @BindingAdapter("plan_problem_02")
+    @BindingAdapter("plan_problem_tow")
     @JvmStatic
     fun TextView.y(t: String?) {
         try {
-            t?.let {
-                text = t.substring(1,t.length)
-            }
+
         } catch (_: Exception) {
         }
     }
@@ -373,12 +239,7 @@ object a {
     @JvmStatic
     fun TextView.t(t: String?) {
         try {
-            t?.let {
-                val s = "¥${it}"
-                val sp = SpannableString(s)
-                sp.setSpan(StrikethroughSpan(),0,s.length,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-                text = sp
-            }
+
         } catch (_: Exception) {
         }
     }
@@ -387,13 +248,7 @@ object a {
     @JvmStatic
     fun View.w(t: Int?) {
         try {
-            t?.let {
-                val w = ScreenUtils.getScreenWidth()-DpiUtils.dip2px(Utils.getContext(),52)
-                val w1 = w*t/100
-                val lp: ViewGroup.LayoutParams = layoutParams
-                lp.width = w1
-                layoutParams = lp
-            }
+
         } catch (_: Exception) {
         }
     }
@@ -402,18 +257,7 @@ object a {
     @JvmStatic
     fun View.q(t: Int?) {
         try {
-            t?.let {
-                if (it == 0){
-                    visibility = View.GONE
-                }else{
-                    visibility = View.VISIBLE
-                    val w = ScreenUtils.getScreenWidth()-DpiUtils.dip2px(Utils.getContext(),52)
-                    val w1 = w*it/100-DpiUtils.dip2px(Utils.getContext(),20)
-                    val layoutParams = layoutParams as ViewGroup.MarginLayoutParams
-                    layoutParams.setMargins(w1,0,0,0)
-                    setLayoutParams(layoutParams)
-                }
-            }
+
         } catch (_: Exception) {
         }
     }
@@ -425,39 +269,7 @@ object a {
     @JvmStatic
     fun RulerView.ab(a: String?, b:String?, g:String?, v:String?) {
         try {
-            setValue(v?.let {
-                if (it.isNotEmpty()){
-                    it.toFloat()
-                }else{
-                    60f
-                }
-            }?: kotlin.run {
-                60f
-            }, a?.let {
-                if (it.isNotEmpty()){
-                    it.toFloat()
-                }else{
-                    0f
-                }
-            }?: kotlin.run {
-                0f
-            }, b?.let {
-                if (it.isNotEmpty()){
-                    it.toFloat()
-                }else{
-                    200f
-                }
-            }?: kotlin.run {
-                200f
-            }, g?.let {
-                if (it.isNotEmpty()){
-                    it.toFloat()/10f
-                }else{
-                    0.1f
-                }
-            }?: kotlin.run {
-                0.1f
-            })
+
         } catch (_: Exception) {
         }
     }
@@ -471,9 +283,7 @@ object a {
         a: BindingCommand<String?>?
     ) {
         try {
-            setOnValueChangeListener{
-                a?.execute(it.toString())
-            }
+
         } catch (_: Exception) {
         }
     }
@@ -485,13 +295,7 @@ object a {
     @JvmStatic
     fun TextView.ad(a: String?, b: String?) {
         try {
-            a?.let {
-                text = it.ifEmpty {
-                    b
-                }
-            }?: kotlin.run {
-                text = b
-            }
+
         } catch (_: Exception) {
         }
     }
@@ -503,19 +307,7 @@ object a {
     @JvmStatic
     fun TextView.bd(a: String?, b: String?) {
         try {
-            text = if (b?.isNotEmpty() == true) {
-                if (a?.isNotEmpty() == true){
-                    StringUtils.format(b, a.toDouble().roundToInt().toString())
-                }else{
-                    StringUtils.format(b, "0")
-                }
-            }else{
-                if (a?.isNotEmpty() == true){
-                    a.toDouble().roundToInt().toString()
-                }else{
-                    "0"
-                }
-            }
+
         } catch (_: Exception) {
         }
     }
@@ -524,15 +316,7 @@ object a {
     @JvmStatic
     fun TextView.ae(t: String?) {
         try {
-            t?.let {
-                text = if (it.isNotEmpty()){
-                    "${it.length}"
-                }else{
-                    "0"
-                }
-            }?: kotlin.run {
-                text = "0"
-            }
+
         } catch (_: Exception) {
         }
     }
